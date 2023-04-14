51c261c3cdec6e3dc56ee23ba246c8060c56b786
Hugo Caron <hca443@gmail.com>
2023-04-14 18:01:00 +0200

    add some tree-sitter module

--- nvim/nvim-setup.sh
+++ nvim/nvim-setup.sh
@@ -4,6 +4,10 @@ if ! command -v node &> /dev/null
 then
 	echo "installing NVM then node"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 	nvm install node
 	npm i -g pyright
 fi
