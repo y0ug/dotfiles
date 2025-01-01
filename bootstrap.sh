5711a14a75c4c05e21784e9b5c0aed403f633c67
Hugo Caron <hca443@gmail.com>
2025-01-01 08:07:39 +0100

    Update boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
-apt update && apt install sudo curl wget git
+export PATH=$HOME/.local/bin:$PATH
+apt update && apt install sudo curl wget git jq
 CHEZMOI_MODE=home
 sh -c "$(curl -fsLS https://raw.githubusercontent.com/y0ug/dotfiles/refs/heads/main/install.sh)"
 sh -c "$(wget -qO- https://raw.githubusercontent.com/y0ug/dotfiles/refs/heads/main/install.sh)"
