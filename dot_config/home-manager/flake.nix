501127c0df1c9e6267a36a5caa94b27c81ebbff4
Hugo Caron <hca443@gmail.com>
2025-01-04 07:53:18 +0100

    Update .config/home-manager/flake.nix

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -5,49 +5,56 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # systems.url = "github:nix-systems/default";
-    # flake-utils = {
-    #   url = "github:numtide/flake-utils";
-    # };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }:
     let
-
+      system = "x86_64-linux";
+      # user = "rick";
+      # user = import ./user.nix;
+      lib = nixpkgs.lib;
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = _: true;
+        };
+      };
+      #nixpkgs.legacyPackages.${system};
     in
     {
-      # nixosConfigurations = {
-      #   rick = nixpkgs.lib.nixosSystem {
-      #     inherit system;
-      #     modules = [
-      #       ./configuration.nix
-      #       home-manager.nixosModules.home-manager
-      #       {
-      #         home-manager.useGlobalPkgs = true;
-      #         home-manager.useUserPackages = true;
-      #         home-manager.users.rick = import ./home.nix;
-      #       }
-      #     ];
-      #   };
-      # };
-      homeConfigurations."rick@linux" = home-manager.lib.homeManagerConfiguration {
-          # inherit pkgs system;
-          modules = [ ./home.nix ];
-          # extraSpecialArgs = { inherit inputs pkgs; };
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-          };
-        };
+      homeConfigurations = {
 
-      # homeConfigurations."rick@osx" = home-manager.lib.homeManagerConfiguration {
-      #     # inherit pkgs system;
-      #     modules = [ ./home.nix ];
-      #     # extraSpecialArgs = { inherit inputs pkgs; };
-      #     pkgs = import nixpkgs {
-      #       system = "aarch64-darwin";
-      #     };
-      #   };
+        rick = inputs.home-manager.lib.homeManagerConfiguration {
+          #   # Specify the host architecture
+          inherit pkgs;
+          #
+          #   # Specify your home configuration modules here, for example,
+          #   # the path to your home.nix.
+          modules = [ ./home.nix ];
+          #
+          #   extraSpecialArgs = { inherit inputs; };
+        };
+      };
+      nixosConfigurations = {
+        rick = nixpkgs.lib.nixosSystem {
+          inherit system;
+          # specialArgs = attrs // { pkgs = pkgs };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              # home-manager.users.root = import ./home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+        };
+      };
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
     };
 }
 
