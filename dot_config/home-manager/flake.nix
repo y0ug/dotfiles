f07ec9f4e46b789b392366c2d0dacd19e62c5638
Hugo Caron <hca443@gmail.com>
2025-01-04 07:56:13 +0100

    Update .config/home-manager/flake.nix

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-          flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }:
