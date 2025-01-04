ab2020885cfa6e68e377d7cf6b060cd12aafaa15
Hugo Caron <hca443@gmail.com>
2025-01-04 07:46:28 +0100

    Update .config/home-manager/flake.nix

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -16,31 +16,39 @@
   outputs = inputs@{ nixpkgs, home-manager, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-      # pkgs = import nixpkgs {
-      #   inherit system;
-      # };
+
     in
     {
-      nixosConfigurations = {
-        rick = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.rick = import ./home.nix;
-            }
-          ];
-        };
-      };
-      homeConfigurations.${system}.rick = home-manager.lib.homeManagerConfiguration {
+      # nixosConfigurations = {
+      #   rick = nixpkgs.lib.nixosSystem {
+      #     inherit system;
+      #     modules = [
+      #       ./configuration.nix
+      #       home-manager.nixosModules.home-manager
+      #       {
+      #         home-manager.useGlobalPkgs = true;
+      #         home-manager.useUserPackages = true;
+      #         home-manager.users.rick = import ./home.nix;
+      #       }
+      #     ];
+      #   };
+      # };
+      homeConfigurations."rick@linux" = home-manager.lib.homeManagerConfiguration {
           # inherit pkgs system;
-          pkgs = nixpkgs.legacyPackages.${system};
           modules = [ ./home.nix ];
           # extraSpecialArgs = { inherit inputs pkgs; };
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+          };
+        };
+
+      homeConfigurations."rick@linux" = home-manager.lib.homeManagerConfiguration {
+          # inherit pkgs system;
+          modules = [ ./home.nix ];
+          # extraSpecialArgs = { inherit inputs pkgs; };
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+          };
         };
     });
 }
