a9c9875e234bf68297db17f8eddfcd47f4a09aad
Hugo Caron <hca443@gmail.com>
2025-02-05 20:57:49 +0100

    Update .local/bin/rdp-lab-wk002.sh

--- dot_local/bin/rdp-lab-wk002.sh
+++ dot_local/bin/rdp-lab-wk002.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-bwc.py --fields=password microsoft | \
-  run.sh xfreerdp  /v:10.83.20.102 \  
-  /dynamic-resolution /d:MicrosoftAccount \
-  /u:hca443@gmail.com /floatbar /clipboard \
-  /from-stdin
+bwc.py --fields=password microsoft |
+  run.sh xfreerdp /v:10.83.20.102 \
+    /dynamic-resolution /d:MicrosoftAccount \
+    /u:hca443@gmail.com /floatbar /clipboard \
+    /from-stdin
