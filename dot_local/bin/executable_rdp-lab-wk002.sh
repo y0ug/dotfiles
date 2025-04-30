822196683297307192f46bedcd99a260c2b71ca6
Hugo Caron <hca443@gmail.com>
2025-04-30 23:41:38 +0200

    Change attributes of .local/bin/rdp-lab-wk002.sh

--- dot_local/bin/executable_rdp-lab-wk002.sh
+++ dot_local/bin/executable_rdp-lab-wk002.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 bwc.py --fields=password microsoft |
-  run.sh wlfreerdp /v:10.83.20.102 \
+  run.sh xfreerdp /v:10.83.20.102 \
     /dynamic-resolution /d:MicrosoftAccount \
     /u:hca443@gmail.com /floatbar /clipboard \
-    /f -grab-keyboard -grab-mouse \
-    /from-stdin
+    /from-stdin /multimon
+# /size:1920x1024 \
+
+#-grab-keyboard -grab-mouse \
