cab8914df86d7a24880b9b94bbad50ae430d4398
Caddy Glow <caddyglow@pm.me>
2026-04-22 21:14:49 +0200

    feat(shell): add image resize helper
    - Add a shell function for quick image downscaling
    - Default resized output to 2048px with stripped metadata

--- dot_config/shell/functions.sh
+++ dot_config/shell/functions.sh
@@ -15,3 +15,8 @@ touchf() { mkdir -p "$(dirname "$1")" && touch "$1"; }
 cliprun() { eval "$(wl-paste)"; }
 fdcat() { fd -t f "$@" -x sh -c 'printf "\n\033[1;34m=== %s ===\033[0m\n" "$1" && cat "$1"' _ {}; }
 clone() { git clone "https://github.com/$1.git" "${2:-${1##*/}}"; }
+
+resize() {
+  local size="${2:-2048}"
+  magick "$1" -resize "${size}x${size}" -strip -quality 85 "resized_$1"
+}
